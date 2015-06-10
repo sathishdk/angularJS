8dcf789949c8511d2413fbb1e649444fc8fa2bd7
Sathish DK <sathishdk@sathishdk.com>
2015-06-11 04:06:33 +0530

    Angular JS App

---
+++ scripts/controllers/mainController.js
@@ -0,0 +1,30 @@
+var artistController = angular.module("artistController", ['ngAnimate']);
+
+artistController.controller("ListController", ['$scope', '$http' , function($scope, $http){
+	$http.get('scripts/data.json').success(function(data){
+    $scope.artists = data;
+    $scope.artistOrder = 'name';
+  });  
+}]);
+
+artistController.controller("DetailsController", ['$scope', '$http', '$routeParams' , function($scope, $http, $routeParams){
+  $http.get('scripts/data.json').success(function(data){
+    $scope.artists = data;
+    $scope.whichItem = $routeParams.itemId;
+
+    if ($routeParams.itemId > 0) {
+      $scope.prevItem = Number($routeParams.itemId) - 1;
+    }
+    else {
+      $scope.prevItem = $scope.artists.length;
+    }
+
+    if ($routeParams.itemId < $scope.artists.length-1) {
+      $scope.nextItem = Number($routeParams.itemId) + 1;
+    }
+    else {
+      $scope.nextItem = 0;
+    }
+
+  });  
+}]);
